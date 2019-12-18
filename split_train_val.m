b72685d0f6d3341ca160687400b3e7a3bae4072b
e.osterhed <e.osterhed@campus.unimib.it>
2019-12-18 14:36:08 +0100

    Delete categories with few images

--- split_train_val.m
+++ split_train_val.m
@@ -1,10 +1,11 @@
-[imagesIds, ~, masterCategory, subCategory] = loadstyles();
+[imagesIds, ~, masterCategory, subCategory, articleType] = loadstyles();
 
 missing_images = find_missing_images(imagesIds);
 [~, idxs] = ismember(missing_images, imagesIds);
 imagesIds(idxs.',:) = []; % remove missing images
 masterCategory(idxs.',:) = [];
 subCategory(idxs.',:) = [];
+articleType(idxs.',:) = [];
 
 % Se vogliamo rimuovere le immagini solo grigie
 % gray_images = find_gray_images(imagesIds);
@@ -14,10 +15,103 @@ subCategory(idxs.',:) = [];
 % subCategory(idxs.',:) = [];
 
 % Rimuoviamo l'immagine relativa al label Home che è solo una
-pos = find(strcmp(masterCategory,'Home'));
+pos = [];
+pos = [pos; find(strcmp(masterCategory,'Home'))];
+pos = [pos; find(strcmp(articleType, 'Body Wash and Scrub'))];
+pos = [pos; find(strcmp(articleType, 'Cushion Covers'))];
+pos = [pos; find(strcmp(articleType, 'Hair Accessory'))];
+pos = [pos; find(strcmp(articleType, 'Ipad'))];
+pos = [pos; find(strcmp(articleType, 'Mens Grooming Kit'))];
+pos = [pos; find(strcmp(articleType, 'Shoe Laces'))];
+pos = [pos; find(strcmp(articleType, 'Suits'))];
+pos = [pos; find(strcmp(articleType, 'Face Serum and Gel'))];
+pos = [pos; find(strcmp(articleType, 'Key chain'))];
+pos = [pos; find(strcmp(articleType, 'Rain Trousers'))];
+pos = [pos; find(strcmp(articleType, 'Ties and Cufflinks'))];
+pos = [pos; find(strcmp(articleType, 'Hat'))];
+pos = [pos; find(strcmp(articleType, 'Lounge Tshirts'))];
+pos = [pos; find(strcmp(articleType, 'Tablet Sleeve'))];
+pos = [pos; find(strcmp(articleType, 'Trolley Bag'))];
+pos = [pos; find(strcmp(articleType, 'Beauty Accessory'))];
+pos = [pos; find(strcmp(articleType, 'Lehenga Choli'))];
+pos = [pos; find(strcmp(articleType, 'Lip Plumper'))];
+pos = [pos; find(strcmp(articleType, 'Makeup Remover'))];
+pos = [pos; find(strcmp(articleType, 'Robe'))];
+pos = [pos; find(strcmp(articleType, 'Nehru Jackets'))];
+pos = [pos; find(strcmp(articleType, 'Toner'))];
+pos = [pos; find(strcmp(articleType, 'Face Scrub and Exfoliator'))];
+pos = [pos; find(strcmp(articleType, 'Body Lotion'))];
+pos = [pos; find(strcmp(articleType, 'Eye Cream'))];
+pos = [pos; find(strcmp(articleType, 'Nail Essentials'))];
+pos = [pos; find(strcmp(articleType, 'Shrug'))];
+pos = [pos; find(strcmp(articleType, 'Umbrellas'))];
+pos = [pos; find(strcmp(articleType, 'Headband'))];
+pos = [pos; find(strcmp(articleType, 'Salwar and Dupatta'))];
+pos = [pos; find(strcmp(articleType, 'Wristbands'))];
+pos = [pos; find(strcmp(articleType, 'Blazers'))];
+pos = [pos; find(strcmp(articleType, 'Clothing Set'))];
+pos = [pos; find(strcmp(articleType, 'Footballs'))];
+pos = [pos; find(strcmp(articleType, 'Shapewear'))];
+pos = [pos; find(strcmp(articleType, 'Tights'))];
+pos = [pos; find(strcmp(articleType, 'Concealer'))];
+pos = [pos; find(strcmp(articleType, 'Rucksacks'))];
+pos = [pos; find(strcmp(articleType, 'Water Bottle'))];
+pos = [pos; find(strcmp(articleType, 'Booties'))];
+pos = [pos; find(strcmp(articleType, 'Mask and Peel'))];
+pos = [pos; find(strcmp(articleType, 'Rompers'))];
+pos = [pos; find(strcmp(articleType, 'Basketballs'))];
+pos = [pos; find(strcmp(articleType, 'Mascara'))];
+pos = [pos; find(strcmp(articleType, 'Waistcoat'))];
+pos = [pos; find(strcmp(articleType, 'Baby Dolls'))];
+pos = [pos; find(strcmp(articleType, 'Jumpsuit'))];
+pos = [pos; find(strcmp(articleType, 'Lip Care'))];
+pos = [pos; find(strcmp(articleType, 'Travel Accessory'))];
+pos = [pos; find(strcmp(articleType, 'Swimwear'))];
+pos = [pos; find(strcmp(articleType, 'Waist Pouch'))];
+pos = [pos; find(strcmp(articleType, 'Rain Jacket'))];
+pos = [pos; find(strcmp(articleType, 'Hair Colour'))];
+pos = [pos; find(strcmp(articleType, 'Bath Robe'))];
+pos = [pos; find(strcmp(articleType, 'Gloves'))];
+pos = [pos; find(strcmp(articleType, 'Shoe Accessories'))];
+pos = [pos; find(strcmp(articleType, 'Sunscreen'))];
+pos = [pos; find(strcmp(articleType, 'Face Wash and Cleanser'))];
+pos = [pos; find(strcmp(articleType, 'Tracksuits'))];
+pos = [pos; find(strcmp(articleType, 'Churidar'))];
+pos = [pos; find(strcmp(articleType, 'Salwar'))];
+pos = [pos; find(strcmp(articleType, 'Stockings'))];
+pos = [pos; find(strcmp(articleType, 'Jeggings'))];
+pos = [pos; find(strcmp(articleType, 'Lounge Shorts'))];
+pos = [pos; find(strcmp(articleType, 'Mufflers'))];
+pos = [pos; find(strcmp(articleType, 'Patiala'))];
+pos = [pos; find(strcmp(articleType, 'Camisoles'))];
+pos = [pos; find(strcmp(articleType, 'Suspenders'))];
+pos = [pos; find(strcmp(articleType, 'Eyeshadow'))];
+pos = [pos; find(strcmp(articleType, 'Messenger Bag'))];
+pos = [pos; find(strcmp(articleType, 'Mobile Pouch'))];
+pos = [pos; find(strcmp(articleType, 'Lip Liner'))];
+pos = [pos; find(strcmp(articleType, 'Compact'))];
+pos = [pos; find(strcmp(articleType, 'Boxers'))];
+pos = [pos; find(strcmp(articleType, 'Highlighter and Blush'))];
+pos = [pos; find(strcmp(articleType, 'Fragrance Gift Set'))];
+pos = [pos; find(strcmp(articleType, 'Jewellery Set'))];
+pos = [pos; find(strcmp(articleType, 'Face Moisturisers'))];
+pos = [pos; find(strcmp(articleType, 'Lounge Pants'))];
+pos = [pos; find(strcmp(articleType, 'Bracelet'))];
+pos = [pos; find(strcmp(articleType, 'Sports Sandals'))];
+pos = [pos; find(strcmp(articleType, 'Foundation and Primer'))];
+pos = [pos; find(strcmp(articleType, 'Laptop Bag'))];
+pos = [pos; find(strcmp(articleType, 'Bangle'))];
+pos = [pos; find(strcmp(articleType, 'Duffel Bag'))];
+pos = [pos; find(strcmp(articleType, 'Stoles'))];
+pos = [pos; find(strcmp(articleType, 'Free Gifts'))];
+pos = [pos; find(strcmp(articleType, 'Kurta Sets'))];
+pos = [pos; find(strcmp(subCategory, 'Perfumes'))];
+pos = [pos; find(strcmp(subCategory, 'Free Gifts'))];
+pos = [pos; find(strcmp(subCategory, 'Eyes'))];
 imagesIds(pos,:) = [];
 masterCategory(pos,:) = [];
 subCategory(pos,:) = [];
+articleType(pos,:) = [];
 
 % Create ImageDatastore
 location = cellstr(strcat('../images/',num2str(imagesIds),'.jpg'));
@@ -29,19 +123,6 @@ save('imds.mat','imdsMaster','imdsSub');
 % Per vedere la distribuzione
 % T = countEachLabel(imdsMaster)
 
-cvMaster = cvpartition(masterCategory, 'KFold', 3);
-cvSub = cvpartition(subCategory, 'KFold', 3);
-save('cvpartitions.mat','cvMaster','cvSub');
-
-% Semplice split
-
-% numImages = size(imagesIds,1);
-% valSize = 0.25;
-% 
-% trainIndexes = randperm(numImages, floor((1 - valSize)*numImages));
-% valIndexes = setdiff(1:numImages,trainIndexes);
-% 
-% trainIds = imagesIds(trainIndexes);
-% valIds = imagesIds(valIndexes);
-% 
-% save('train_val_ids.mat','trainIds','valIds');
+% cvMaster = cvpartition(masterCategory, 'KFold', 3);
+% cvSub = cvpartition(subCategory, 'KFold', 3);
+% save('cvpartitions.mat','cvMaster','cvSub');
