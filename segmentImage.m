955fab589c304a6750292af9e53ce86cb1b823e5
e.osterhed <e.osterhed@campus.unimib.it>
2019-12-21 20:00:04 +0100

    Correct some issues in segmentation

--- segmentImage.m
+++ segmentImage.m
@@ -5,7 +5,11 @@ function segmentedImage = segmentImage(image)
     else
         gray = im;
     end
-    
+
+    T_distance = 0.25; %Threshold distance from center
+    center = [size(gray,2) / 2, size(gray,1) / 2];
+    max_distance = [abs(T_distance * size(gray,2) - center(1))
+                    abs(T_distance * size(gray,1) - center(2))];
     T_area = 0.75; %Threshold area
     max_area = T_area * size(gray,1) * size(gray,2);
     
@@ -31,28 +35,38 @@ function segmentedImage = segmentImage(image)
         compl = bw3;
     end
 %     imshow(compl);
-    compl = imclearborder(compl); % oppure prendere elemento più centrale
+    %compl = imclearborder(compl); % oppure prendere elemento più centrale
 
     stats = regionprops(compl,'BoundingBox');
     bbox = cell2mat({stats.BoundingBox}.');
     sortedBbox = sortrows(bbox,[3 4],'descend');
-    acceptedBbox = discardBboxes(sortedBbox, max_area);
-     
-    bb = acceptedBbox(1,:);
+    acceptedBbox = discardBboxes(sortedBbox, max_area, center, max_distance);
+    
+    if size(acceptedBbox,1) > 0
+        bb = acceptedBbox(1,:);
 %     figure,imshow(compl),hold on;
 %     rectangle('Position',bb,'EdgeColor','r');
 
-    segmentedImage = imcrop(im,bb);
+        segmentedImage = imcrop(im,bb);
+    else
+        segmentedImage = image;
+    end
 end
 
-function acceptedBbox = discardBboxes(bbox, max_area)
+function acceptedBbox = discardBboxes(bbox, max_area, center, max_distance)
+    minRatio = 0.5;
+    maxRatio = 1.5;
     idx = [];
     for ii = 1:size(bbox,1)
+        centerX = bbox(ii,1);
+        centerY = bbox(ii,2);
         width = bbox(ii,3);
         height = bbox(ii,4);
         ratio = width / height;
         area = width * height;
-        if ratio < 0.5 || ratio > 1.5 || area >= max_area
+        distanceX = abs(center(1) - centerX);
+        distanceY = abs(center(2) - centerY);
+        if ratio < minRatio || ratio > maxRatio || area >= max_area || distanceX > max_distance(1) || distanceY > max_distance(2)
             idx = [idx; ii];
         end
     end
--- testSegment.m
+++ testSegment.m
@@ -6,18 +6,18 @@
 % segmentImage(im2);
 % segmentImage(im3);
 
-im = imread('testImages/test2.jpg');
-s = segmentImage(im);
+% im = imread('testImages/test10.jpg');
+% s = segmentImage(im);
 
-% testdir = 'testImages';
-% files = dir(testdir);
-% if exist('testSegment','dir') ~= 7
-%     mkdir testSegment;
-% end
-% for f = 3:length(files)
-%     filename = files(f).name;
-%     fprintf("Segmenting %s\n",filename);
-%     im = imread(strcat(testdir,'/',filename));
-%     res = segmentImage(im);
-%     imwrite(res,strcat('testSegment/seg_',filename));
-% end
+testdir = 'testImages';
+files = dir(testdir);
+if exist('testSegment','dir') ~= 7
+    mkdir testSegment;
+end
+for f = 3:length(files)
+    filename = files(f).name;
+    fprintf("Segmenting %s\n",filename);
+    im = imread(strcat(testdir,'/',filename));
+    res = segmentImage(im);
+    imwrite(res,strcat('testSegment/seg_',filename));
+end
