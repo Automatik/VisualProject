079a505ebdb2dfd3c362c0f1501ddff41b33f206
e.osterhed <e.osterhed@campus.unimib.it>
2019-12-21 16:05:33 +0100

    Create method to segment fashion items
    Still need to fix some issues

--- segmentImage.m
+++ segmentImage.m
@@ -1,15 +1,61 @@
-im1 = imread('../test1.jpg');
-im2 = imread('../test2.jpg');
+function segmentedImage = segmentImage(image)
+    im = im2double(image);
+    if size(image,3) == 3
+        gray = rgb2gray(im);
+    else
+        gray = im;
+    end
+    
+    T_area = 0.75; %Threshold area
+    max_area = T_area * size(gray,1) * size(gray,2);
+    
+    % rimozione rumore / artefatti
+    F = fspecial('gaussian', 15, 1.5);
+    filtered = imfilter(gray,F);
+    
+    if mean2(filtered) > 0.4
+        hasWhiteBackground = true;
+    else
+        hasWhiteBackground = false;
+    end
+    
+    bw = filtered > 0.7; %altrimenti usare adaptthresh
+    bw2 = imclose(bw, strel('disk',7,4));
+    bw3 = bwareaopen(bw2,1000);
+    
+    % lo schermo del monitor è necessario rimanga in primo piano,
+    % altrimenti sbaglia a capire il colore del background
+    if hasWhiteBackground
+        compl = imcomplement(bw3);
+    else
+        compl = bw3;
+    end
+%     imshow(compl);
+    compl = imclearborder(compl); % oppure prendere elemento più centrale
 
-g1 = rgb2gray(im1);
-g2 = rgb2gray(im2);
+    stats = regionprops(compl,'BoundingBox');
+    bbox = cell2mat({stats.BoundingBox}.');
+    sortedBbox = sortrows(bbox,[3 4],'descend');
+    acceptedBbox = discardBboxes(sortedBbox, max_area);
+     
+    bb = acceptedBbox(1,:);
+%     figure,imshow(compl),hold on;
+%     rectangle('Position',bb,'EdgeColor','r');
 
-%rimuove artefatti immagini
-bw = g1 > 180;
-close = imclose(bw, strel('disk',7,4));
-compl = imcomplement(close);
-imshow(compl);
+    segmentedImage = imcrop(im,bb);
+end
 
-stats = regionprops(compl,'BoundingBox');
-figure,imshow(compl),hold on;
-rectangle('Position',stats(82).BoundingBox,'EdgeColor','r');
+function acceptedBbox = discardBboxes(bbox, max_area)
+    idx = [];
+    for ii = 1:size(bbox,1)
+        width = bbox(ii,3);
+        height = bbox(ii,4);
+        ratio = width / height;
+        area = width * height;
+        if ratio < 0.5 || ratio > 1.5 || area >= max_area
+            idx = [idx; ii];
+        end
+    end
+    acceptedBbox = bbox;
+    acceptedBbox(idx,:) = [];
+end
